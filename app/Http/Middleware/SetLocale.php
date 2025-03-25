f68745b27475c305b8120f299cea83404cedbc08
Eap Vanny <eapvanny16@gmail.com>
2025-03-25 10:09:44 +0700

    update

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -2,11 +2,8 @@
 
 namespace App\Http\Middleware;
 
-use App\Models\Role;
-use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class SetLocale
@@ -18,30 +15,12 @@ class SetLocale
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // dd(Auth::user());
-
-        if(!empty(Auth::user())){
-            $check_role = Role::find(@Auth::user()->id)->get()->first();
-            // dd($check_role->name);
-            if($check_role->name == 'Employee'){
-                app()->setLocale('kh'); 
-            }else{
-                if (auth()->check() && auth()->user()->user_lang) {
-                    app()->setLocale(auth()->user()->user_lang); // Set the language
-                } else {
-                    app()->setLocale('kh');  // Default language if not set
-                }
-            }
-        }else{
-            if (auth()->check() && auth()->user()->user_lang) {
-                app()->setLocale(auth()->user()->user_lang); // Set the language
-            } else {
-                app()->setLocale('kh');  // Default language if not set
-            }
-        }
-
         // Check if the user is authenticated and has a language preference
-        
+        if (auth()->check() && auth()->user()->user_lang) {
+            app()->setLocale(auth()->user()->user_lang); // Set the language
+        } else {
+            app()->setLocale('kh');  // Default language if not set
+        }
 
         return $next($request);
     }
