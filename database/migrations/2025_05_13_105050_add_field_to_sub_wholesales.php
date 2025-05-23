6fd311f1ee27d5e839d537ca7cdceca1ef6dbde7
Eap Vanny <eapvanny16@gmail.com>
2025-05-23 15:18:49 +0700

    add filed

---
+++ database/migrations/2025_05_13_105050_add_field_to_sub_wholesales.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('sub_wholesales', function (Blueprint $table) {
+            $table->longText('photo_foc')->nullable()->after('photo');
+            $table->string('foc_qty')->nullable()->after('photo_foc');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('sub_wholesales', function (Blueprint $table) {
+            $table->dropColumn('photo_foc');
+            $table->dropColumn('foc_qty');
+        });
+    }
+};
