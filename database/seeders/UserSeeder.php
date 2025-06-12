d0c1f1120be6b465c3f89a29f4b984e20ff69526
Eap Vanny <eapvanny16@gmail.com>
2025-06-12 14:42:30 +0700

    update seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -84,6 +84,18 @@ class UserSeeder extends Seeder
                 'view se',
                 'update se',
                 'delete se',
+                'create school',
+                'view school',
+                'update school',
+                'delete school',
+                'create restaurant',
+                'view restaurant',
+                'update restaurant',
+                'delete restaurant',
+                'create sport club',
+                'view sport club',
+                'update sport club',
+                'delete sport club',
                 'create permission',
                 'view permission',
                 'update permission',
@@ -91,6 +103,36 @@ class UserSeeder extends Seeder
                 'view dashboard',
                 'view setting',
                 'reset password',
+            ],
+            AppHelper::SALE => [
+                'create customer',
+                'view customer',
+                'update customer',
+                'delete customer',
+                'create report',
+                'view report',
+                'update report',
+                'delete report',
+                'create user',
+                'view user',
+                'update user',
+                'delete user',
+                'view dashboard',
+            ],
+            AppHelper::SE => [
+                'create customer',
+                'view customer',
+                'update customer',
+                'delete customer',
+                'create report',
+                'view report',
+                'update report',
+                'delete report',
+                'create user',
+                'view user',
+                'update user',
+                'delete user',
+                'view dashboard',
             ],
         ];
 
@@ -112,53 +154,5 @@ class UserSeeder extends Seeder
             }
         }
 
-        // Create sample users for SE and SALE types
-        // if (!User::where('email', 'se_user@gmail.com')->exists()) {
-        //     $seUser = User::create([
-        //         'username' => 'se_user',
-        //         'email' => 'se_user@gmail.com',
-        //         'password' => Hash::make('crm123'),
-        //         'staff_id_card' => '1281',
-        //         'family_name' => 'សុខ',
-        //         'name' => 'សុភ័ក្ត្រ',
-        //         'family_name_latin' => 'Sok',
-        //         'name_latin' => 'Sophea',
-        //         'position' => 'Supervisor',
-        //         'area' => 'ភ្នំពេញ',
-        //         'role_id' => AppHelper::USER_SUP,
-        //         'phone_no' => '098123456',
-        //         'type' => AppHelper::SE,
-        //     ]);
-
-        //     $seUser->syncRoles($roles[AppHelper::USER_SUP]->name);
-        //     UserRole::create([
-        //         'user_id' => $seUser->id,
-        //         'role_id' => $roles[AppHelper::USER_SUP]->id,
-        //     ]);
-        // }
-
-        // if (!User::where('email', 'sale_user@gmail.com')->exists()) {
-        //     $saleUser = User::create([
-        //         'username' => 'sale_user',
-        //         'email' => 'sale_user@gmail.com',
-        //         'password' => Hash::make('crm123'),
-        //         'staff_id_card' => '1282',
-        //         'family_name' => 'គឹម',
-        //         'name' => 'សុធា',
-        //         'family_name_latin' => 'Kim',
-        //         'name_latin' => 'Sothea',
-        //         'position' => 'Employee',
-        //         'area' => 'សៀមរាប',
-        //         'role_id' => AppHelper::USER_EMPLOYEE,
-        //         'phone_no' => '098654321',
-        //         'type' => AppHelper::SALE,
-        //     ]);
-
-        //     $saleUser->syncRoles($roles[AppHelper::USER_EMPLOYEE]->name);
-        //     UserRole::create([
-        //         'user_id' => $saleUser->id,
-        //         'role_id' => $roles[AppHelper::USER_EMPLOYEE]->id,
-        //     ]);
-        // }
     }
 }
