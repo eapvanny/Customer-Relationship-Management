c97571f56d906433c52fb56a9281591b9e5a10c0
Eap Vanny <eapvanny16@gmail.com>
2025-06-24 10:27:15 +0700

    update translate and change photo report

--- resources/views/backend/report/list.blade.php
+++ resources/views/backend/report/list.blade.php
@@ -535,17 +535,17 @@
                             </div>
                         </div>
                         <div class="col-md-12 col-sm-12 col-lg-12 col-xl-6 mt-2">
-                            <img style="border: 1px solid #cfcfcf;" id="modalPhoto" src=""
-                                class="img-fluid photo-detail" alt="Photo Detail">
-                            <div class="text-center mt-3">
-                                <b>{{ __('POSM Photo') }}</b>
-                            </div>
-                        </div>
-                        <div class="col-md-12 col-sm-12 col-lg-12 col-xl-6 mt-2">
                             <img style="border: 1px solid #cfcfcf;" id="modalPhotoOutlet" src=""
                                 class="img-fluid photo-detail" alt="Photo_outlet Detail">
                             <div class="text-center mt-3">
                                 <b>{{ __('Outlet Photo') }}</b>
+                            </div>
+                        </div>
+                        <div class="col-md-12 col-sm-12 col-lg-12 col-xl-6 mt-2">
+                            <img style="border: 1px solid #cfcfcf;" id="modalPhoto" src=""
+                                class="img-fluid photo-detail" alt="Photo Detail">
+                            <div class="text-center mt-3">
+                                <b>{{ __('POSM Photo') }}</b>
                             </div>
                         </div>
                     </div>
