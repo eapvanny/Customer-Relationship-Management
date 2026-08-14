14bf335dfaf0ab55fd00332bc19638384292506d
Eap Vanny <eapvanny16@gmail.com>
2026-08-14 10:11:40 +0700

    update get user profile

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -17,26 +17,31 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::with('role')->get()->map(function ($user) {
-            return [
-                'id'            => $user->id,
-                'staff_id_card' => $user->staff_id_card,
-                'username'      => $user->username,
-                'area'          => $user->area,
-                'role'          => $user->role?->name, // or $user->role?->title
-                'phone_no'      => $user->phone_no,
-                'name'          => $user->full_name,
-                'gender'        => AppHelper::GENDER[$user->gender] ?? 'N/A',
-                'email'         => $user->email,
-                'photo'         => $user->photo
-                    ? asset('storage/' . $user->photo)
-                    : asset('images/avatar.png'),
-            ];
-        });
+        $authUser = auth()->user();
+
+        $users = User::with('role')
+            ->where('id', $authUser->id)
+            ->get()
+            ->map(function ($user) {
+                return [
+                    'id'            => $user->id,
+                    'staff_id_card' => $user->staff_id_card,
+                    'username'      => $user->username,
+                    'area'           => $user->area,
+                    'role'           => $user->role?->name,
+                    'phone_no'      => $user->phone_no,
+                    'name'          => $user->full_name,
+                    'gender'        => AppHelper::GENDER[$user->gender] ?? 'N/A',
+                    'email'         => $user->email,
+                    'photo'         => $user->photo
+                        ? asset('storage/' . $user->photo)
+                        : asset('images/avatar.png'),
+                ];
+            });
 
         return response()->json([
             'success' => true,
-            'data'    => $users
+            'data'    => $users,
         ]);
     }
 
