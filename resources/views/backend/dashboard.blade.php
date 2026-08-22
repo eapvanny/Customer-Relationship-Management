232a217d7501bcccd31dd832ec71d7796e3e2d7a
Eap Vanny <eapvanny16@gmail.com>
2026-08-22 11:24:38 +0700

    update translate

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -623,7 +623,7 @@
                                             class="crm-btn crm-btn-clear"
                                         >
                                             <i class="fas fa-times"></i>
-                                            <span>{{ __('Clear') }}</span>
+                                            <span>{{ __('Cancel') }}</span>
                                         </a>
 
                                     </div>
@@ -660,12 +660,12 @@
                     <h3 id="all-user" style="font-weight: 900;">{{ $allUsers }}</h3>
                 </div>
             </div>
-            <div class="col-lg-6 col-xl-3 col-md-6 col-sm-6">
+            {{-- <div class="col-lg-6 col-xl-3 col-md-6 col-sm-6">
                 <div class="card" style="color: rgb(60, 60, 60); font-size: xx-large; border-bottom: 3px solid #39a1ea;">
                     <h6 style="font-weight: 900;">{{ __('User Active') }}</h6>
                     <h3 id="all-report" style="font-weight: 900;">{{ $userActive }}</h3>
                 </div>
-            </div>
+            </div> --}}
         </div>
     </section>
     <section>
