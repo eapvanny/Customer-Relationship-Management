b77379c4e3697d28fcd309d4493691358f2e83a2
Eap Vanny <eapvanny16@gmail.com>
2026-08-25 10:58:50 +0700

    update show report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -590,7 +590,7 @@ class ReportController extends Controller
                 //  Phone safe
                 'phone' => optional($report->customer)->phone ?? 'N/A',
 
-                'city' => $report->city ?? $report->address ?? 'N/A',
+                'city' => trim(($report->city ?? '') . ' ' . ($report->address ?? '')) ?: 'N/A',
                 'posm' => $posm,
                 'qty' => $report->qty,
 
